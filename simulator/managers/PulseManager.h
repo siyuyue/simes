97a7520301023fb4e704baa064d576506c924ef8
Siyu Yue <ysysamuel@gmail.com>
2013-11-20 11:38:51 -0800

    Add pulse load and pulse manager.

---
+++ simulator/managers/PulseManager.h
@@ -0,0 +1,32 @@
+// **********************************************
+// Copyright (c) 2013 SPORTS Lab(http://atrak.usc.edu/~sport/),
+// University of Southern California
+//
+// CPulseManager class:
+// - Inherited from ChargeManagerBase
+// - Does nothing
+// **********************************************
+#include "ChargeManager.h"
+
+class CPulseManager : public CChargeManagerBase
+{
+private:
+    double _nextDecisionTime;
+    double _period;
+    double _superCapEnergyTargetHigh;
+    double _superCapEnergyTargetLow;
+    double _periodHi;
+    double _periodLow;
+    double _nextPeriodTime;
+    bool _mode;      // Low = 0, High = 1
+    string _superCapBankName;
+public:
+    CPulseManager(void);
+    virtual ~CPulseManager(void);
+    virtual bool IsDecisionEpoch(double time) const;
+    virtual void Decision(double time, vector<CLoadBase*> pLoads, vector<CBankBase*> pBanks, vector<CSourceBase*> pSources, vector<CConverterBase*> pConverters, vector<CCTI*> pCTIs);
+    virtual void Reset();
+    virtual double NextTimeStep(double time, int precision) const;
+    virtual void TimeElapse(double time, double timeElapsed);
+    virtual bool SetProperty(const string &name, const string &value);
+};
